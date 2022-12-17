f84fafc612d7fce4e6ac1ee6c4ea93cc56a2adbf
Hernandez, Lysander <lysanderhernandez@my.unt.edu>
2022-12-16 18:16:10 -0600

    styling

--- components/NavBar/NavBarComponent.tsx
+++ components/NavBar/NavBarComponent.tsx
@@ -2,6 +2,17 @@
 
 import styles from "./NavBarComponent.module.scss"
 import {AppBar, Button, Link, Toolbar} from "@mui/material";
+import {ReactNode} from "react";
+import EmailIcon from '@mui/icons-material/Email'
+import CallIcon from '@mui/icons-material/Call';
+
+function NavBarButton({children}: {children: ReactNode}) {
+	return (
+		<Button variant="text" className={styles.linkButton} color="secondary">
+			{children}
+	</Button>
+	)
+}
 
 export default function NavBarComponent() {
 	return (
@@ -10,24 +21,39 @@ export default function NavBarComponent() {
 
 			<Toolbar className={styles.navToolbar}>
 
+				<div className={`${styles.iconText} ${styles.email}`}>
+					<EmailIcon className={styles.icon} />
+					<span className={`${styles.text}`}>
+						hernandezlysander22@gmail.com
+					</span>
+
+				</div>
+
+				<div className={`${styles.iconText} ${styles.phone}`}>
+					<CallIcon className={styles.icon} />
+					<a className={`${styles.text}`} href={`tel:+14696553521`}>+1 (469)-655-3521</a>
+				</div>
+
 				<div className={styles.linkGroup}>
+
 					<Link href="#top">
-						<Button variant="text" className={styles.linkButton} color="secondary">
+						<NavBarButton>
 							Top
-						</Button>
+						</NavBarButton>
 					</Link>
 
 					<Link href="#ibm">
-						<Button variant="text" className={styles.linkButton} color="secondary">
+						<NavBarButton>
 							IBM
-						</Button>
+						</NavBarButton>
 					</Link>
 
 					<Link href="#tritech">
-						<Button variant="text" className={styles.linkButton} color="secondary">
+						<NavBarButton>
 							TriTech
-						</Button>
+						</NavBarButton>
 					</Link>
+
 				</div>
 
 			</Toolbar>
