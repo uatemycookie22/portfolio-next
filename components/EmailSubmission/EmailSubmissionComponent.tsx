debe6cfafda0b2763c3425b0d571bec3c431b357
Hernandez, Lysander <lysanderhernandez@my.unt.edu>
2022-12-21 02:10:06 -0600

    onInvalid

--- components/EmailSubmission/EmailSubmissionComponent.tsx
+++ components/EmailSubmission/EmailSubmissionComponent.tsx
@@ -3,13 +3,20 @@
 import {useCallback, useState} from "react";
 import {Button, TextField} from "@mui/material";
 import colors from "/styles/colors.module.scss";
+import {useRouter} from "next/navigation";
+
+const emailRegex = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+const bodyErrorMessage = 'Must be at least 8 characters.'
 
 export default function EmailSubmission() {
 	const [emailContact, setEmail] = useState('')
 	const [emailMessage, setMessage] = useState('')
+	const [errorMessage, setErrorMessage] = useState('')
+
+	const router = useRouter()
 
 	const sendEmail = useCallback(async () => {
-		await fetch('http://127.0.0.1:8090/api/collections/messages/records', {
+		const res = await fetch('http://127.0.0.1:8090/api/collections/messages/records', {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json',},
 			body: JSON.stringify({
@@ -18,10 +25,23 @@ export default function EmailSubmission() {
 			}),
 		})
 
+		const newErrorMessage = res.ok ? '' : bodyErrorMessage
+		setErrorMessage(newErrorMessage)
 
-	}, [emailContact, emailMessage])
+		setMessage('')
+		router.refresh()
+	}, [emailContact, emailMessage, router])
 
-	return (<form style={{display: 'flex', flexDirection: 'column', gap: '25px', maxWidth: '500px' }  } onSubmit={() => sendEmail()}>
+	return (
+		<form style={{display: 'flex', flexDirection: 'column', gap: '25px', maxWidth: '500px' }  }
+          onSubmit={async (e) => {
+						e.preventDefault()
+            await sendEmail()
+          }}
+					onInvalid={() => {
+						setErrorMessage(bodyErrorMessage)
+					}}
+		>
 
 		<h1 style={{
 			color: colors.mainTextColor
@@ -39,24 +59,42 @@ export default function EmailSubmission() {
 			}}
 			color="secondary"
 
-			inputProps={{ style: {WebkitBoxShadow: `0 0 0 100px ${colors.bgPrimary} inset`}}}
+			inputProps={{
+				style: {WebkitBoxShadow: `0 0 0 100px ${colors.bgPrimary} inset`,},
+				pattern: emailRegex,
+				title: "email@domain.com",
+			}}
 
-			onBlur={(e) => setEmail(e.target.value)}
+			value={emailContact}
+
+			onChange={(e) => setEmail(e.target.value)}
 		/>
 
 		<TextField
-			id="outlined-multiline-static"
-			color="secondary"
-			label="Email body"
+			error={!!errorMessage}
+			required
 			multiline
+			id="outlined-multiline-static"
+			variant="outlined"
+			color="secondary"
+			name="Email body"
+			label="Email body"
+			helperText={errorMessage}
 			rows={12}
-			onBlur={(e) => setMessage(e.target.value)}
+
+			inputProps={{
+				minLength: 8,
+				maxLength: 1000,
+			}}
+
+			value={emailMessage}
+
+			onChange={(e) => setMessage(e.target.value)}
 		/>
 
-		<Button type="submit"  color="secondary" variant="outlined" style={{alignSelf: 'flex-start'}}
-
-		>
-			Send</Button>
+		<Button type="submit"  color="secondary" variant="outlined" style={{alignSelf: 'flex-start'}}>
+			Send
+		</Button>
 
 	</form>)
 }
