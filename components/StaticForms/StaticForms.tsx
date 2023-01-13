447b8ba8bac59f1f544e3ce538694d80fda7ffb8
Hernandez, Lysander <lysanderhernandez@my.unt.edu>
2023-01-13 16:36:23 -0600

    change netlify attribute to data-netlify

--- components/StaticForms/StaticForms.tsx
+++ components/StaticForms/StaticForms.tsx
@@ -1,7 +1,7 @@
 export default function StaticForm() {
 	{/*Netlify hidden form*/}
 	{/*@ts-ignore*/}
-	return (<form name="contact" netlify netlify-honeypot="bot-field" hidden>
+	return (<form name="contact" data-netlify="true" netlify-honeypot="bot-field" hidden>
 		<input type="text" name="body" />
 		<input type="email" name="address" />
 	</form>)
