7599c4d52bd84d5783e0c3d17a3fe7e26a3c651d
Hernandez, Lysander <lysanderhernandez@my.unt.edu>
2023-01-23 21:37:34 -0600

    quick color fixes

--- components/Education/SchoolProgress/SchoolProgress.tsx
+++ components/Education/SchoolProgress/SchoolProgress.tsx
@@ -7,8 +7,8 @@ const SchoolProgress = () => {
 	return (
 		<>
 			<div className="flex w-full mt-4 items-center gap-x-4">
-				<div className="h-2 w-full rounded-full bg-gray-800">
-					<div className={`transition-all delay-200 duration-700 rounded-full h-full bg-secondary rounded-full
+				<div className="h-2 w-full rounded-full bg-neutral">
+					<div className={`transition-all delay-200 duration-700 rounded-full h-full bg-white rounded-full
 					${barIntersected ? "w-[45%]" : "w-0"}`}
 					     ref={el => {if (el) barRef.current = el}}>
 					</div>
