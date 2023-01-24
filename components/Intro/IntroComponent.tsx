9d702f571dac54d065ec3c180f0b1153096807c8
Hernandez, Lysander <lysanderhernandez@my.unt.edu>
2023-01-23 20:22:16 -0600

    add gradient to hero

--- components/Intro/IntroComponent.tsx
+++ components/Intro/IntroComponent.tsx
@@ -4,7 +4,7 @@ type IntroComponentProps = Parameters<typeof Hero>[0]
 
 export default function IntroComponent(props: IntroComponentProps) {
 	return (<>
-		<section id={"home"}>
+		<section id={"home"} className="bg-gradient-to-t from-primary to-black">
 			<Hero {...props} />
 		</section>
 	</>)
