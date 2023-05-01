eb2618840f991ea0e87c01a4bd28e5396cf2d784
lh <pairofkings30@gmail.com>
2023-05-01 16:31:24 -0500

    decrease card size and scale on hover

--- components/JobCard/JobCard.tsx
+++ components/JobCard/JobCard.tsx
@@ -5,7 +5,7 @@ import {ReactNode, useState} from "react";
 export default function JobCard({ front, back }: {front: ReactNode, back: ReactNode}) {
 	const [flipped, setFlipped] = useState(false);
 
-	return <div className="mb-8 max-w-[28rem] w-full lg:mb-0 transition-all hover:transform-perspective transition-all">
+	return <div className="mb-8 max-w-[25rem] w-full lg:mb-0 transition-all hover:transform-perspective transition-all">
 
 		<button className="sm:h-[512px] min-h-[512px] dark:bg-white rounded-lg shadow-xl shadow-neutral dark:shadow-black transition-all duration-100 relative w-full relative btn flex justify-start " onClick={() => setFlipped(!flipped)}>
 			<div className={`h-full w-full
