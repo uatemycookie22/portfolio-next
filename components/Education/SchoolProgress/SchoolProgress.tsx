8ecadc71ea4d4a6baedcdd1101374c1e7b92eb81
lh <pairofkings30@gmail.com>
2023-05-03 00:59:32 -0500

    add back animation

--- components/Education/SchoolProgress/SchoolProgress.tsx
+++ components/Education/SchoolProgress/SchoolProgress.tsx
@@ -12,9 +12,9 @@ const SchoolProgress = ({percentage}: SchoolProgressProps) => {
 		<>
 			<div className="flex w-full mt-4 items-center gap-x-4">
 				<div className="h-2 w-full rounded-full bg-opacity-50 bg-zinc-500 dark:bg-neutral">
-					<div className={`transition-size delay-200 duration-700 rounded-full h-full bg-violet-600 dark:bg-white rounded-full
-					${barIntersected ? `w-[65%]` : "w-0"}`}
-						 style={{width: `${percentage}%`}}
+					<div className={`transition-size delay-200 duration-700 rounded-full h-full 
+					bg-violet-600 dark:bg-white rounded-full`}
+						 style={{width: `${barIntersected ? percentage : 0}%`}}
 					     ref={el => {if (el) barRef.current = el}}>
 					</div>
 				</div>
