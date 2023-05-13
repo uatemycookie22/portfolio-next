cee30c37ff4c15fbf218d3942a7f28e7e09e387a
lh <pairofkings30@gmail.com>
2023-05-13 00:59:00 -0500

    Big: replaced netlify forms with pocketbase db

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -1,12 +1,32 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import {pb} from "../../pocketbase/pocketbase";
+import {ClientResponseError} from "pocketbase";
 
-const minMessageLength = 8
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const {body}: {body: string} = req.body
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const {body, address}: {body: string, address: string} = req.body
 
-    if (body.length < minMessageLength) {
-        res.status(400).json({error: 'Message must be 8 characters or longer.'})
+    try {
+        const record = await pb.collection('messages').create({
+            message: body,
+            from: address,
+        })
+        console.log(record)
+
+        res.status(200).end()
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error)
+
+            if (isClientResponseError(error)) {
+                const {message: pbMessage} = Object.entries(error.response.data)[0][1] as {message: string}
+                res.status(error.response.code).json({error: pbMessage})
+            }
+        }
+
+        res.status(500).end()
     }
+}
 
-    res.status(200).end()
+function isClientResponseError(error: Error): error is ClientResponseError {
+    return 'url' in error && 'response' in error && 'data' in error
 }
