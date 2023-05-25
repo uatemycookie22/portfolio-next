df40061ab55333f7dc5b4b54b8958ea9a5064d41
uatemycookie <pairofkings30@gmail.com>
2023-05-25 17:33:38 -0500

    add icons to jobcard

---
+++ components/WrappedIcons/MAssignmentInd.tsx
@@ -0,0 +1,6 @@
+'use client';
+import {AssignmentInd} from "@mui/icons-material";
+
+export default function MAssignmentInd(props: Parameters<typeof AssignmentInd>[0]) {
+	return (<AssignmentInd {...props} />)
+}
---
+++ components/WrappedIcons/MDateRange.tsx
@@ -0,0 +1,6 @@
+'use client';
+import {DateRange} from "@mui/icons-material";
+
+export default function MDateRange(props: Parameters<typeof DateRange>[0]) {
+	return (<DateRange {...props} />)
+}
