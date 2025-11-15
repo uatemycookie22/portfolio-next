52471f1b08d08d1e8592101d3fb3750f5808c6d3
Lysander Hernandez <lnandez@amazon.com>
2025-11-14 23:46:45 -0600

    Add href icons to anchor tags in blogs

--- src/app/blogs/[...slug]/(blog)/blog-content.tsx
+++ src/app/blogs/[...slug]/(blog)/blog-content.tsx
@@ -1,4 +1,5 @@
 import ReactMarkdown, { MarkdownAsync } from 'react-markdown'
+import { ExternalLink } from 'lucide-react'
 
 export interface BlogContentProps {
     children: string
@@ -8,7 +9,18 @@ export interface BlogContentProps {
 export default async function BlogContent({ children }: BlogContentProps) {
     // MarkdownAsync returns a Promise<ReactElement> - perfect for Suspense
     const content = await MarkdownAsync({
-        children: children.replace(/\\n/g, "\n")
+        children: children.replace(/\\n/g, "\n"),
+        components: {
+            a: ({ href, children, ...props }) => {
+                const isExternal = href?.startsWith('http');
+                return (
+                    <a href={href} {...props} className="group">
+                        <span>{children}</span>
+                        {isExternal && <span className="inline-block"><ExternalLink className="inline w-4 h-4 ml-1 align-text-bottom text-accent group-hover:text-accent-hover md:text-content md:dark:text-neutral-primary transition-colors duration-[600ms]" /></span>}
+                    </a>
+                );
+            }
+        }
     });
     
     return (
